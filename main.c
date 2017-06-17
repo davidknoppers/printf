19a65a641b8524295e29d7949b4d34b6edc65eae
Jianqin Wang <jianqinwang94@gmail.com>
2017-06-17 13:08:16 -0700

    update documentation, README

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - main function, user can use here to test out the _printf function
+ * Return: 0 for successful completion of program
+ */
+int main(void)
+{
+	_printf("%b\n", 98);
+	return (0);
+}
